3d5ccc4ccaec0316d423d19d0ea8541fba5abf7a
Exequiel <exee.lp@hotmail.com>
2024-07-26 15:25:15 -0300

    eliminarServicioAdmin

--- src/components/ServiceTable.jsx
+++ src/components/ServiceTable.jsx
@@ -106,9 +106,9 @@ export const ServiceTable = ({ onSeleccionarServicio }) => {
                         <td className="m-auto">
                           <button
                             className="tabla-admin-btn admin-btn"
-                            onClick={(e) =>
+                          /*   onClick={(e) =>
                               (eliminarServicioAdmin(e, servicio.id))
-                            }
+                            } */
                           >
                             <img
                               className="admin-icons"
