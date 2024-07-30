432c8acac0a5135450cdf8416d7df43a8b096e85
Ezequiel <ezequielarce_1996@hotmail.com>
2024-07-29 23:48:32 -0300

    feat(user-table): Add navlink to user-table

--- src/components/ListaTurnos.jsx
+++ src/components/ListaTurnos.jsx
@@ -4,54 +4,63 @@ import TurnoReservado from "./TurnoReservado";
 import "./css/listaTurno.css";
 
 const ListaTurnos = () => {
-  const { arrayTurnos } = useContext(ContextoAdministrador);
+  const { arrayTurnos, usuarioLogeado } = useContext(ContextoAdministrador);
   return (
-    <div className="accordion" id="accordionExample">
-      <div className="accordion-item">
-        <h2 className="accordion-header" id="headingOne">
-          <button
-            className="accordion-button"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapseOne"
-            aria-expanded="true"
-            aria-controls="collapseOne"
+    <section className="tabla-user">
+      <h1 className="user-title">
+        ¡Bienvenido{" "}
+        <span className="user-userName">{usuarioLogeado.userName}</span>!
+      </h1>
+      <h3 className="user-subtitle">
+        Consulta o modifica tus turnos reservados aquí 👇
+      </h3>
+      <div className="accordion" id="accordionExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingOne">
+            <button
+              className="accordion-button"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="true"
+              aria-controls="collapseOne"
+            >
+              Turnos reservados
+            </button>
+          </h2>
+          <div
+            id="collapseOne"
+            className="accordion-collapse collapse show"
+            data-bs-parent="#accordionExample"
           >
-            Turnos reservados
-          </button>
-        </h2>
-        <div
-          id="collapseOne"
-          className="accordion-collapse collapse show"
-          data-bs-parent="#accordionExample"
-        >
-          <div className="accordion-body">
-            {arrayTurnos &&
-            arrayTurnos.length > 0 &&
-            arrayTurnos.some((e) => e.estado === true) ? (
-              <table className="table align-middle">
-                <thead className="tabla-header">
-                  <tr>
-                    <th scope="col">Nombre del servicio</th>
-                    <th scope="col">Hora del turno</th>
+            <div className="accordion-body">
+              {arrayTurnos &&
+              arrayTurnos.length > 0 &&
+              arrayTurnos.some((e) => e.estado === true) ? (
+                <table className="table align-middle">
+                  <thead className="tabla-header">
+                    <tr>
+                      <th scope="col">Nombre del servicio</th>
+                      <th scope="col">Hora del turno</th>
 
-                    <th scope="col">Costo</th>
-                    <th scope="col">Modificar</th>
-                  </tr>
-                </thead>
-                {arrayTurnos.map((e) =>
-                  e.estado === true ? (
-                    <TurnoReservado listaTurnos={e} key={e.id} />
-                  ) : null
-                )}
-              </table>
-            ) : (
-              <p>No tienes turnos reservados</p>
-            )}
+                      <th scope="col">Costo</th>
+                      <th scope="col">Modificar</th>
+                    </tr>
+                  </thead>
+                  {arrayTurnos.map((e) =>
+                    e.estado === true ? (
+                      <TurnoReservado listaTurnos={e} key={e.id} />
+                    ) : null
+                  )}
+                </table>
+              ) : (
+                <p>No tienes turnos reservados</p>
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
