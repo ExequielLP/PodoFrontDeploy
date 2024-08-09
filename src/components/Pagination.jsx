94f67083cb65daad0e68a43f8ff9b8d3de38044a
Ezequiel <ezequielarce_1996@hotmail.com>
2024-08-08 22:52:32 -0300

    feat(pagination): update component  to fix pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,24 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ResponsivePagination from "react-responsive-pagination";
-
 import "react-responsive-pagination/themes/classic.css";
-// ^ classic theme, see below for other themes
-// include this once in the project (preferrably the main index.js)
 
-export default function Pagination() {
-  const totalPages = 120;
-  const [currentPage, setCurrentPage] = useState(1);
+export default function Pagination({ page, totalPages, onPageChange }) {
+  const [currentPage, setCurrentPage] = useState(page);
+
+  useEffect(() => {
+    setCurrentPage(page);
+  }, [page]);
 
   function handlePageChange(page) {
+    console.log(page);
     setCurrentPage(page);
-    // ... do something with `page`
+    onPageChange(page); // Llama a la función de cambio de página del componente padre
   }
 
   return (
     <ResponsivePagination
       total={totalPages}
       current={currentPage}
-      onPageChange={(page) => handlePageChange(page)}
+      onPageChange={handlePageChange}
     />
   );
 }
