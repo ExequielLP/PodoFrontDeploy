a27e1f4c1a94acf4d486e0968f8a169e75b65e8e
Ezequiel <ezequielarce_1996@hotmail.com>
2024-10-03 19:52:16 -0300

    feat(Title): correct the title name

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -5,7 +5,7 @@ export default function useTitle({ title }) {
 
   useEffect(() => {
     const previusTitle = prevTitle.current;
-    document.title = `${title} | Podo Estética LP`;
+    document.title = `${title} | Pedicuría La Plata`;
 
     return () => (document.title = previusTitle);
   }, [title]);
