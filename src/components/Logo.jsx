6cf2b04c4f70bba843b9fd67d6acf90a5dd4d102
Ezequiel <ezequielarce_1996@hotmail.com>
2024-10-03 19:29:32 -0300

    feat(logo): create logo component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import "./css/logo.css"
+
+export const Logo = ({ height = 80, width = 80 }) => {
+  return (
+    <img
+    className="logo"
+    src="/assets/ImagenesOptimizadas/Pedicuria-la-plata.webp"
+    alt="Pedicuría La Plata"
+    width={width}
+    height={height}
+  />
+  )
+}
