f2326d47d037f49202b8124aac48ad90a7ef98f0
Ezequiel <ezequielarce_1996@hotmail.com>
2024-10-23 18:59:18 -0300

    feat(dashboard): improve and render home dashboard

--- src/Admin/pages/Dashboard.jsx
+++ src/Admin/pages/Dashboard.jsx
@@ -1,22 +1,38 @@
-import { useEffect } from "react";
-import ServicesContext from "../../context/ServiceContext";
+import { Suspense } from "react";
 import useContextValue from "../../hooks/useContextValue";
 import useTitle from "../../hooks/useTitle";
-import "./css/Dashboard.css";
+import styles from "./css/Dashboard.module.css";
+import { CardsSkeleton } from "../../shared/skeleton/CardSkeleton";
+import { RevenueChartSkeleton } from "../../shared/skeleton/RevenueChartSkeleton";
+import { LatestInvoicesSkeleton } from "../../shared/skeleton/InvoiceSkeleton";
 import AuthenticationContext from "../../context/AuthContext";
+import { ChartView } from "../components/ChartView";
+import { LatestAppointments } from "../components/LatestAppointments";
+import CardWrapper from "../components/CardWrapper";
 
 const Dashboard = () => {
   useTitle({ title: "Panel Administrador" });
-  const { arrayTurnosAdmin } = useContextValue(ServicesContext);
-const { usuarioLogueado}= useContextValue(AuthenticationContext);
-
-  useEffect(() => {}, [arrayTurnosAdmin]);
+  const { usuarioLogueado } = useContextValue(AuthenticationContext);
 
   return (
-    <main className="dashboard-section">
-      <h1 className="admin-title">
-        ¡Bienvenido <span className="admin-userName">{usuarioLogueado.userName}</span>!
+    <main className={styles.main}>
+      {/* ACA VAN LAS BREADCRUMBS */}
+      <h1 className={`${styles.dashboardTitle} ${styles.dashboardTitleMd}`}>
+        Dashboard {usuarioLogueado.userName}
       </h1>
+      <div className={`${styles.gridContainer} ${styles.gridCols2} ${styles.gridCols4}`}>
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
+      </div>
+      <div className={`${styles.marginTop6} ${styles.gridContainer} ${styles.gridCols1} ${styles.gridCols4Md} ${styles.gridCols8}`}>
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <ChartView />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestAppointments />
+        </Suspense>
+      </div>
     </main>
   );
 };
