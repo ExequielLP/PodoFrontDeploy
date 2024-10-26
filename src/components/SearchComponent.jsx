684c05200c3c5ec3a0f8f9285798100aaa74e534
Ezequiel <ezequielarce_1996@hotmail.com>
2024-10-26 01:17:08 -0300

    Feat(Filter-search): create a preview of filter modal

--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -1,20 +1,19 @@
 import { useState } from "react";
 import "./css/search-component.css";
 import { FilterIcon, SearchIcon } from "../icons";
+import { useModalContext } from "../context/ModalContext";
+import { Modal } from "../shared/components/Modal";
+import FilterComponent from "../shared/components/FilterrComponent";
 
-const services = [
-  { value: "LÁSER DE ALTA FRECUENCIA", label: "LÁSER DE ALTA FRECUENCIA" },
-  { value: "MÁQUINA DE MASAJES DE PIES", label: "MÁQUINA DE MASAJES DE PIES" },
-  { value: "ONICOMICOSIS", label: "ONICOMICOSIS" },
-  { value: "REFLEXOLOGÍA", label: "REFLEXOLOGÍA" },
-  { value: "SERVICIO GENERAL", label: "SERVICIO GENERAL" },
-  { value: "SPA DE PIES", label: "SPA DE PIES" },
-];
-const servicesOrderASC = services.sort((a, b) => (a.value > b.value ? 1 : -1));
+
 
 const SearchComponent = ({ searchType, onSearch }) => {
   const [searchValue, setSearchValue] = useState("");
-  const [selectedService, setSelectedService] = useState("");
+  const { state, setState } = useModalContext();
+
+  const openModal = (turno) => {
+    setState(true);
+  };
 
   const handleChange = (event) => {
     setSearchValue(event.target.value);
@@ -24,9 +23,6 @@ const SearchComponent = ({ searchType, onSearch }) => {
     onSearch(searchValue);
   };
 
-  const handleServiceChange = (value) => {
-    setSelectedService(value);
-  };
 
   return (
     <div className="search-container">
@@ -37,30 +33,15 @@ const SearchComponent = ({ searchType, onSearch }) => {
         onChange={handleChange}
         className="search-input"
       />
-      <button className="actionsButton" onClick={handleSearch}>
+      <button className="actionsButton" onClick={handleSearch} title="Buscar">
         <SearchIcon color="#000" size={20} />
       </button>
-      <div className="select-container">
-        <FilterIcon color="#fff" size={20} />
-        <select
-          value={selectedService}
-          onChange={handleServiceChange}
-          className="search-filter"
-        >
-          <option value="" disabled>
-            Filtrar por servicio
-          </option>
-          {servicesOrderASC.map((service) => (
-            <option
-              className="search-selector"
-              key={service.value}
-              value={service.value}
-            >
-              {service.label}
-            </option>
-          ))}
-        </select>
-      </div>
+      <button className="actionsButton" onClick={openModal} title="Filtros">
+        <FilterIcon color="#000" size={20} />
+      </button>
+      <Modal>
+        <FilterComponent />
+      </Modal>
     </div>
   );
 };
