5cad8bd64c69d338e8f79ef6329e50078a6887cc
Ezequiel <ezequielarce_1996@hotmail.com>
2024-10-25 23:11:06 -0300

    feat(search-component): terminar de modificar el componente de busqueda....

--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import './css/search-component.css'
+import "./css/search-component.css";
+import { FilterIcon, SearchIcon } from "../icons";
 
 const services = [
   { value: "LÁSER DE ALTA FRECUENCIA", label: "LÁSER DE ALTA FRECUENCIA" },
@@ -8,51 +9,60 @@ const services = [
   { value: "REFLEXOLOGÍA", label: "REFLEXOLOGÍA" },
   { value: "SERVICIO GENERAL", label: "SERVICIO GENERAL" },
   { value: "SPA DE PIES", label: "SPA DE PIES" },
-]
-const servicesOrderASC= services.sort((a, b) => (a.value > b.value ? 1 : -1));
+];
+const servicesOrderASC = services.sort((a, b) => (a.value > b.value ? 1 : -1));
 
-const SearchComponent = ({searchType, onSearch}) => {
-    const [searchValue, setSearchValue] = useState('')
-    const [selectedService, setSelectedService] = useState('')
+const SearchComponent = ({ searchType, onSearch }) => {
+  const [searchValue, setSearchValue] = useState("");
+  const [selectedService, setSelectedService] = useState("");
 
-    const handleChange = (event) => {
-        setSearchValue(event.target.value);
-      };
-    
-      const handleSearch = () => {
-        onSearch(searchValue);
-      };
+  const handleChange = (event) => {
+    setSearchValue(event.target.value);
+  };
 
-      const handleServiceChange = (value) => {
-        setSelectedService(value)
-      }
+  const handleSearch = () => {
+    onSearch(searchValue);
+  };
+
+  const handleServiceChange = (value) => {
+    setSelectedService(value);
+  };
 
   return (
     <div className="search-container">
-        <input
+      <input
         type="text"
         placeholder={`Buscar ${searchType}`}
         value={searchValue}
         onChange={handleChange}
         className="search-input"
-        />
-        <button className="admin-btn" onClick={handleSearch}>Buscar</button>
+      />
+      <button className="actionsButton" onClick={handleSearch}>
+        <SearchIcon color="#000" size={20} />
+      </button>
+      <div className="select-container">
+        <FilterIcon color="#fff" size={20} />
         <select
-        value={selectedService}
-        onChange={handleServiceChange}
-        className="search-filter"
+          value={selectedService}
+          onChange={handleServiceChange}
+          className="search-filter"
         >
-          <option value="" disabled placeholder="Filtrar por servicio">
+          <option value="" disabled>
             Filtrar por servicio
           </option>
           {servicesOrderASC.map((service) => (
-            <option className="search-selector" key={service.value} value={service.value}>
+            <option
+              className="search-selector"
+              key={service.value}
+              value={service.value}
+            >
               {service.label}
             </option>
           ))}
         </select>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default SearchComponent;
